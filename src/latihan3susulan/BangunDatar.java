fd1447421a637e50c34c240bc67af5c001529dd0
fansa543 <alffadilah76@gmail.com>
2025-06-24 15:42:03 +0700

    innitial commit

---
+++ src/latihan3susulan/BangunDatar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan3susulan;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BangunDatar {
+    // Nama  : Al-Fadilah Nur Sahdan Al-Biya
+// NPM   : 2310010465
+// Kelas : I4C
+
+
+}
