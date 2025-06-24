5ea9b3ed768fb96444af881f9f2125e84ad3cbab
fansa543 <alffadilah76@gmail.com>
2025-06-24 16:00:41 +0700

    membuat main class

--- src/latihan3susulan/BangunDatarBeraksi.java
+++ src/latihan3susulan/BangunDatarBeraksi.java
@@ -10,5 +10,21 @@ package latihan3susulan;
  * @author LENOVO
  */
 public class BangunDatarBeraksi {
-    
+    public static void main(String[] args) {
+        // Membuat objek dari class BangunDatar dengan nama objek: fadil
+        BangunDatar fadil = new BangunDatar();
+
+        // Mengisi nilai alas, tinggi, dan jarijari
+        fadil.alas = 10;
+        fadil.tinggi = 5;
+        fadil.jarijari = 7;
+
+        // Menghitung dan menampilkan luas segitiga
+        fadil.hitungLuasSegitiga();
+        System.out.println("Nilai hasil (luas segitiga): " + fadil.getHasil());
+
+        // Menghitung dan menampilkan luas lingkaran
+        fadil.hitungLuasLingkaran();
+        System.out.println("Nilai hasil (luas lingkaran): " + fadil.getHasil());
+    }
 }
